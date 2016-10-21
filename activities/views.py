d4a3a8986ebc65dfa02a7b3e258a148282288f13
Sergio Infante <raulsergio9@gmail.com>
2016-10-21 15:19:39 -0500

    Add arduino integration to device endpoint

--- activities/views.py
+++ activities/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets, response, status
 from rest_framework.decorators import detail_route
+from pyfirmata import Arduino
 from .models import Activity
 from .serializer import ActivitySerializer
+
+PORT = '/dev/cu.usbmodem1421'
+
+board = Arduino(PORT)
 
 class ActivityViewset(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
@@ -12,9 +17,11 @@ class DeviceViewset(viewsets.ViewSet):
     @detail_route()
     def interact(self, request, pk=None):
         pin = int(pk)
-        if pin != 1:
+        if board.digital[pin].read() != 1:
+            board.digital[pin].write(1)
             log = "User %s turns ON pin %s" % (request.user, pk)
         else:
+            board.digital[pin].write(0)
             log = "User %s turns OFF pin %s" % (request.user, pk)
         activity = Activity.objects.create(log=log, user=request.user)
         serializer = ActivitySerializer(activity)
