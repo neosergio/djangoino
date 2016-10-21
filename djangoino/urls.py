ff551c5e1d06c6e33f6eec8e936b688b53470530
Sergio Infante <raulsergio9@gmail.com>
2016-10-21 14:19:14 -0500

    Add schema view to enable swagger for better documentation

--- djangoino/urls.py
+++ djangoino/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Django Arduino API')
 
 urlpatterns = [
+    url(r'^$', schema_view),
     url(r'^admin/', admin.site.urls),
 ]
